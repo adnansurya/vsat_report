5c72545c3eecd4a95e0e41acb8ee0c30d88459c9
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-07-30 19:16:40 +0800

    creating blank template

---
+++ blank.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include('partials/head.php'); ?>
+        <title>Static Navigation - SB Admin</title>
+        
+    </head>
+    <body>
+        <?php include('partials/topbar.php'); ?>
+        <div id="layoutSidenav">
+            <?php include('partials/sidebar.php'); ?>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h1 class="mt-4">Static Navigation</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Static Navigation</li>
+                        </ol>
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <p class="mb-0">
+                                    This page is an example of using static navigation. By removing the
+                                    <code>.sb-nav-fixed</code>
+                                    class from the
+                                    <code>body</code>
+                                    , the top navigation and side navigation will become static on scroll. Scroll down this page to see an example.
+                                </p>
+                            </div>
+                        </div>
+                        <div style="height: 100vh;"></div>
+                        <div class="card mb-4"><div class="card-body">When scrolling, the navigation stays at the top of the page. This is the end of the static navigation demo.</div></div>
+                    </div>
+                </main>
+                <?php include('partials/footer.php'); ?>
+            </div>
+        </div>
+        <?php include('partials/scripts.php'); ?>
+    </body>
+</html>
