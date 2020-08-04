0ddb306455c2d11ba92a639cf4cab255b180ffdc
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-08-04 21:29:30 +0800

    join same table multiple time
    SELF JOIN

--- histori.php
+++ histori.php
@@ -11,7 +11,7 @@
             }
         ?>
         
-        <title><?php echo $webname; ?> - Blank</title>        
+        <title><?php echo $webname; ?> - Histori</title>        
     </head>
     <body>
         <?php include('partials/topbar.php'); ?>
@@ -25,7 +25,7 @@
                             <div class="mt-4 list-group">
                             <?php
                         
-                                $load = mysqli_query($conn, "SELECT user.* , report.* FROM report INNER JOIN user ON report.customer_id = user.user_id WHERE user.user_id = ".$row['user_id']." ORDER BY report_id DESC");   
+                                $load = mysqli_query($conn, "SELECT cust.nama AS 'nama_cust', admin.nama AS 'nama_admin', admin.hp AS 'hp_admin', report.* FROM user cust, user admin, report WHERE cust.user_id = report.customer_id AND admin.user_id = report.admin_id AND report.customer_id = ".$row['user_id']." ORDER BY report_id DESC");   
                                 while ($row = mysqli_fetch_array($load)){
                                     if($row['stat'] === 'Belum Diproses'){
                                         echo '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">';
@@ -46,7 +46,7 @@
                                                 <div class="card card-body">
                                                     <div class="d-flex w-100 justify-content-between">
                                                         <p class="mb-1">Admin</p>
-                                                        <small><b>Mas Agus - 08123456789</b></small>
+                                                        <small><b>'.$row['nama_admin'].' - '.$row['hp_admin'].'</b></small>
                                                     </div>
                                                     <small>Jenis Gangguan : Pointing bergeser </small>
                                                     <small>Perangkat : SAT182038</small>                                                                                                                       
@@ -73,57 +73,7 @@
                                     }                                
                                 }
                             ?>
-                                <a href="#collapseExample" data-toggle="collapse" class="list-group-item list-group-item-action flex-column align-items-start">
-                                    <div class="d-flex w-100 justify-content-between">
-                                        <h5 class="mb-1">Sedang Diproses</h5>
-                                        <small>2020-07-30 23:32:33</small>
-                                    </div>
-                                    <p class="mb-1">Jl. Toddopuli 5 setapak 1 No. 5</p>
-                                    <small>Data tidak dapat terkirim</small>
-                                </a>
-                                <div class="collapse ml-3 mr-3" id="collapseExample">
-                                    <div class="card card-body">
-                                        <div class="d-flex w-100 justify-content-between">
-                                            <p class="mb-1">Admin</p>
-                                            <small><b>Mas Agus - 08123456789</b></small>
-                                        </div>
-                                        <small>Jenis Gangguan : Pointing bergeser </small>
-                                        <small>Perangkat : SAT182038</small>                                                                                                                       
-                                    </div>
-                                </div>
-                                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-                                    <div class="d-flex w-100 justify-content-between">
-                                    <h5 class="mb-1">Belum Diproses</h5>
-                                    <small>2020-07-30 23:32:33</small>
-                                    </div>
-                                    <p class="mb-1">Jl. Toddopuli 5 setapak 1 No. 5</p>
-                                    <small>Data tidak dapat terkirim</small>
-                                </a>
-                                <a href="#collapseExample2" data-toggle="collapse" class="list-group-item list-group-item-action flex-column align-items-start">
-                                    <div class="d-flex w-100 justify-content-between">
-                                    <h5 class="mb-1">Selesai</h5>
-                                    <small>2020-07-30 23:32:33</small>
-                                    </div>
-                                    <p class="mb-1">Jl. Toddopuli 5 setapak 1 No. 5</p>
-                                    <small>Data tidak dapat terkirim</small>
-                                </a>
-                                <div class="collapse ml-3 mr-3" id="collapseExample2">
-                                    <div class="card card-body">
-                                        <div class="d-flex w-100 justify-content-between">
-                                            <p class="mb-1">Admin</p>
-                                            <small><b>Mas Agus - 08123456789</b></small>
-                                        </div>
-                                        <small>Jenis Gangguan : Pointing bergeser </small>
-                                        <small>Perangkat : SAT182038</small>  
-                                        <hr>
-                                        <div class="d-flex w-100 justify-content-between">
-                                            <p class="mb-1">Teknisi</p>
-                                            <small><b>Mas Pras - 08987654321</b></small>
-                                        </div>
-                                        <small>Tindakan Perbaikan : Rapatkan ulang baut dan sekrup mounting </small>                                        
-                                        <img src="report_img/contoh.png" class="rounded mx-auto d-block mt-3" alt="..." height="200px" style="max-width: 100%;  height: auto;">                                                                                                                      
-                                    </div>
-                                </div>
+                              
                             </div>                                                  
                     </div>
                 </main>
