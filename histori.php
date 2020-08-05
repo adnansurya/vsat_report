d411ff9686336d06dc2717a33bb03362147dbeac
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-08-05 22:43:30 +0800

    fix display after laporan selesai

--- histori.php
+++ histori.php
@@ -25,7 +25,13 @@
                             <div class="mt-4 list-group">
                             <?php
                         
-                                $load = mysqli_query($conn, "SELECT cust.nama AS 'nama_cust', admin.nama AS 'nama_admin', admin.hp AS 'hp_admin', report.* FROM user cust, user admin, report WHERE cust.user_id = report.customer_id AND (admin.user_id = report.admin_id OR report.admin_id = 0) AND report.customer_id = ".$row['user_id']." GROUP BY report_id ORDER BY report_id DESC");   
+                                $load = mysqli_query($conn, "SELECT tek.nama AS 'nama_tek', tek.hp AS 'hp_tek', 
+                                cust.nama AS 'nama_cust', admin.nama AS 'nama_admin', admin.hp AS 'hp_admin', report.* 
+                                FROM user cust, user admin, user tek, report
+                                WHERE cust.user_id = report.customer_id
+                                AND (admin.user_id = report.admin_id OR report.admin_id = 0) 
+                                AND (tek.user_id = report.teknisi_id OR report.teknisi_id = 0) 
+                                AND report.customer_id = ".$row['user_id']." GROUP BY report_id ORDER BY report_id DESC");   
                                 while ($row = mysqli_fetch_array($load)){
                                     if($row['stat'] === 'Belum Diproses'){
                                         echo '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">';
@@ -69,16 +75,19 @@
                                                 <div class="card card-body">
                                                     <div class="d-flex w-100 justify-content-between">
                                                         <p class="mb-1">Admin</p>
-                                                        <small><b>Mas Agus - 08123456789</b></small>
+                                                        <small><b>'.$row['nama_admin'].' - '.$row['hp_admin'].'</b></small>
                                                     </div>
-                                                    <small>Jenis Gangguan : Pointing bergeser </small>                                                   
+                                                    <small>Jenis Gangguan : '.$row['jenis'].' </small>                                                      
                                                     <hr>
                                                     <div class="d-flex w-100 justify-content-between">
                                                         <p class="mb-1">Teknisi</p>
-                                                        <small><b>Mas Pras - 08987654321</b></small>
+                                                        <small><b>'.$row['nama_tek'].' - '.$row['hp_tek'].'</b></small>
                                                     </div>
-                                                    <small>Tindakan Perbaikan : Rapatkan ulang baut dan sekrup mounting </small>                                        
-                                                    <img src="report_img/contoh.png" class="rounded mx-auto d-block mt-3" alt="..." height="200px" style="max-width: 100%;  height: auto;">                                                                                                                      
+                                                    <small><i class="fas fa-check"></i> '.$row['waktu_selesai'].' </small>  
+                                                    <small>Tindakan Perbaikan : '.$row['tindakan'].' </small>
+                                                                                           
+                                                    <img src="report_img/'.$row['gambar'].'" class="rounded mx-auto d-block mt-3" alt="..." height="200px" style="max-width: 100%;  height: auto;">                                                                                                                      
+                                                    
                                                 </div>
                                             </div>';
                                     }                                
