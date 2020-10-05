b04312585717aee909ba657ea2597ad4e8cd1ebb
Adnan <muh.adnansuryaazis@gmail.com>
2020-10-06 06:57:02 +0800

    add tindakan to dashboard guide

--- index.php
+++ index.php
@@ -39,6 +39,8 @@ include('partials/global.php') ?>
                                     <p>Pada penjelasan modem kode error 3 mengindikasikan bahwa transmitter tidak bisa memancar karena tidak dapat berkomunikasi dengan network communication center.</p>  
                                     <h6>Solusi :</h6>
                                     <p>Kondisi ini terjadi selama ketika penerima mengunci sinyal penerimaan dan normalnya memakan wakru sekitar 10 detik, jika situasinya masih berlanjut, Indoor Transmition Unit atau unit transmisi dalam ruangan dimana itu adalah modem satelit perlu diganti.</p>                              
+                                    <h6>Tindakan :</h6>
+                                    <p>Dilakukan penggantian alat yaitu modem satelit</p>
                                 </p>
                             </div>
                         </div>   
@@ -52,7 +54,9 @@ include('partials/global.php') ?>
                                     <h6>Penyebab :</h6> 
                                     <p>Pada penjelasan modem kode error 7 mengindikasikan bahwa pemancar tidak tersedia karena masalah penyetelan penerima. Adanya sambungan yang kabel yang putus pada bagian pemancar.</p>  
                                     <h6>Solusi :</h6>
-                                    <p>Memeriksa sambungan kabel RG 8koaksial pada sambungan BUC dan modem satelit.</p>                              
+                                    <p>Memeriksa sambungan kabel RG 8koaksial pada sambungan BUC dan modem satelit.</p>    
+                                    <h6>Tindakan :</h6>                          
+                                    <p>Dilakukan penggantian alat yaitu kabel koaksial RG 8</p>
                                 </p>
                             </div>
                         </div>   
@@ -67,6 +71,8 @@ include('partials/global.php') ?>
                                     <p>Pemancar sedang menyesuaikan untuk mengoptimalkan jaringan. Kode TX 9 normalnya bukan merupakan kode gangguan karena kode ini muncul ketika kita melakukan proses ranging ke hub.</p>  
                                     <h6>Solusi :</h6>
                                     <p>Menunggu proses ranging sampai selesai, proses ini berlangsung dengan waktu 1 hingga 2 menit. Ranging merupakan proses untuk pengetesan transmit antenna ke hub.</p>                              
+                                    <h6>Tindakan :</h6>
+                                    <p>Jika proses ranging memakan waktu lebih dari 2 menit maka dilakukan penggantian pada modem satelit.</p>
                                 </p>
                             </div>
                         </div>  
@@ -81,6 +87,8 @@ include('partials/global.php') ?>
                                     <p>Pemancar tidak dapat berkomunikasi/tersambung dengan network communication center. Hal ini terjadi karena nilai dari signal strength atau SQF dari antena berada dibawah angka 45 point dimana yang merupakan standar nilai untuk melakukan proses ranging atau transmit.</p>  
                                     <h6>Solusi :</h6>
                                     <p>Melakukan pointing/pengarahan antena VSAT pada posisi yang benar, hingga mendapatkan nilai signal strength ≥45 point.</p>                              
+                                    <h6>Tindakan :</h6>
+                                    <p>Dilakukan pointing atau pengarahan arah pada antena VSAT</p>
                                 </p>
                             </div>
                         </div>  
@@ -97,6 +105,8 @@ include('partials/global.php') ?>
                                     <p>Mengecek tegangan jaringan kabel pada sambungan modem satelit ke BUC dengan multimeter, tegangan yang dikeluarkan oleh modem ialah berada pada rentang 12-24 volt jika tegangan yang didapatkan berada pada rentang 12-24 volt maka kabel tersebut dalam keadaan normal atau tidak putus. Pemasangan kabel yang tidak rapat juga dapat mempengaruhi proses transmit. </p>                              
                                     <p>Selanjutnya yang diperiksa pada saat penanganan gangguan transmit adalah ODU voltage pada BUC pada modem satelit. Rentang ODU transmit yang bagus ialah di rentang 90 dan 160 point, jika ODU voltage dibawah 90 maka ODU tidak dalam keadaan normal dan dilakukan penggantian. </p>
                                     <p>Selanjutnya adalah mengecek SQF pada modem satelit apabila nilai SQF kurang dari 45 point maka harus dilakukan pointing pada antena karena untuk melakukan transmit dan pengetesan ranging SQF minimal yang harus didapat adalah 45 point. </p>
+                                    <h6>Tindakan :</h6>
+                                    <p>Melakukan penggantian alat pada kabel RG 8 dan BUC</p>
                                 </p>
                             </div>
                         </div> 
@@ -113,6 +123,8 @@ include('partials/global.php') ?>
                                     <p>Penyebab dari gangguan ini terjadi jika arah antena yang tidak sesuai atau antena dalam keadaan rusak, ataupun kabel yang rusak. Kondisi ini terjadi jika  nilai signal strength atau SQF dibawah 30 point.</p>  
                                     <h6>Solusi :</h6>
                                     <p>Melakukan pointing/pengarahan antena VSAT pada posisi yang benar, hingga mendapatkan nilai signal strength ≥45 point. Dan melakukan pengecekan kabel koaksial pada sambungan LNB dan modem satelit.</p>                              
+                                    <h6>Tindakan :</h6>
+                                    <p>Dilakukan pointing atau pengarahan arah pada antena VSAT, dan penggantian alat pada kabel koaksial dan modem satelit</p>
                                 </p>
                             </div>
                         </div>     
@@ -127,6 +139,8 @@ include('partials/global.php') ?>
                                     <p>Penyebab dari gangguan ini terjadi jika arah antena yang tidak sesuai dan mengarah pada satelit yang salah yang tidak sesuai parameter frekuensi ataupun lokasi dari satelit tujuan.</p>  
                                     <h6>Solusi :</h6>
                                     <p>Melakukan pointing/pengarahan antena VSAT pada posisi yang benar, hingga mendapatkan nilai signal strength ≥45 point. Dan melakukan pengecekan kabel koaksial pada sambungan LNB dan modem satelit.</p>                              
+                                    <h6>Tindakan :</h6>
+                                    <p>Dilakukan pointing atau pengarahan arah pada antena VSAT</p>
                                 </p>
                             </div>
                         </div>                          
