985c5fc0ff1dbcc123aac9dcb97eedb06eaf583c
Adnan <muh.adnansuryaazis@gmail.com>
2020-10-09 14:08:49 +0800

    display code error at download

--- download.php
+++ download.php
@@ -10,12 +10,13 @@ if(!isset($_GET['id'])){
     header("location:index.php");   
 }else{
    
-    $report_sql = mysqli_query($conn,"SELECT cust.nama as 'nama_cust', admin.nama as 'nama_admin' , tek.nama as 'nama_tek', report.* , 
+    $report_sql = mysqli_query($conn,"SELECT cust.nama as 'nama_cust', admin.nama as 'nama_admin' , tek.nama as 'nama_tek', report.* , error_list.kode_error, 
     dev1.device_name as nama_dev1 , dev2.device_name as nama_dev2, dev3.device_name as nama_dev3 
-    FROM user cust, user admin, user tek, report, device dev1, device dev2, device dev3 WHERE 
+    FROM user cust, user admin, user tek, report, error_list, device dev1, device dev2, device dev3 WHERE 
     report.customer_id = cust.user_id AND 
     report.teknisi_id = tek.user_id AND 
     report.admin_id = admin.user_id AND 
+    (report.error_id = error_list.id_error OR report.error_id = 0) AND
     (report.device_id = dev1.device_id OR report.device_id = 0) AND 
     (report.device2_id = dev2.device_id OR report.device2_id = 0) AND 
     (report.device3_id = dev3.device_id OR report.device3_id = 0) AND 
@@ -63,6 +64,8 @@ if(!isset($_GET['id'])){
             <p id="adminTxt"><?php echo $report['nama_admin']; ?></p>
             <small>Jenis Gangguan</small>
             <p id="jenisTxt"><?php echo $report['jenis']; ?></p>
+            <small>Kode Error</small>
+            <p id="errorTxt"><?php echo $report['kode_error']; ?></p>
             <small>List Perangkat :</small>
             <ul>
                 <li id="dev1Txt"><?php echo $report['nama_dev1']; ?></li>
